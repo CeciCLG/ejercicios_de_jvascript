cc7adc6ba535c11f297ec36e4e6e305ceb67cfae
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-05-11 20:37:22 +0200

    finish ejercicio3

--- S3/1.dom-selectors/exercise-3.js
+++ S3/1.dom-selectors/exercise-3.js
@@ -1,2 +1,8 @@
-let p = document.querySelector(p);
+let testP = document.querySelectorAll(`p`);
 
+
+console.log(testP);
+
+for (let i = 0; i < testP.length; i++) {
+    console.log(testP[i].outerText);
+}
