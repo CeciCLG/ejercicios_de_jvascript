3e0762041690c0c2556fe0fc0fc21685036e16ca
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-06-06 19:13:21 +0200

    creación de la seccion 7

--- S7/2.destructuring/exercise-1.js
+++ S7/2.destructuring/exercise-1.js
@@ -1,6 +1,11 @@
 const game = {
-    title: 'The last us 2', 
-    gender: ['action', 'zombie', 'survival'], 
-    year: 2020}
+    title: 'The last us 2',
+    gender: ['action', 'zombie', 'survival'],
+    year: 2020
+}
 
-let {title, gender, year} = game;
+let { title, gender, year } = game;
+
+console.log(`Título ${title}`);
+console.log(`Género ${gender}`);
+console.log(`Año ${year}`);
