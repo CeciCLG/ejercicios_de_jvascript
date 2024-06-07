b4a8965225b510faf37143b94706edf9fab2d39a
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-06-07 19:02:28 +0200

    terminado el ejercicio 3

--- S7/4.map/exercise-1.js
+++ S7/4.map/exercise-1.js
@@ -1,21 +1,23 @@
 
 const users = [{
-    id: 1, 
-    name: 'Abel'},
+    id: 1,
+    name: 'Abel'
+},
 {
-    id:2,
+    id: 2,
     name: 'Julia'
-},{
-    id:3, 
+}, {
+    id: 3,
     name: 'Pedro'
 }, {
-    id:4, 
-    name: 'Amanda'}];
+    id: 4,
+    name: 'Amanda'
+}];
 
-users.map((array) => { 
-    let namesArray = [];
-    for (let i = 0, i < array.length, i++) {
-    namesArray.push(array[i].name);
-    }
-    return namesArray
+let namesArray = [];
+
+users.map((user) => {
+    namesArray.push(user.name);
 });
+
+console.log(namesArray);
--- S7/4.map/exercise-2.js
+++ S7/4.map/exercise-2.js
@@ -1,23 +1,31 @@
 const users = [{
-    id: 1, 
-    name: 'Abel'},
+    id: 1,
+    name: 'Abel'
+},
 {
-    id:2,
+    id: 2,
     name: 'Julia'
-},{
-    id:3, 
+}, {
+    id: 3,
     name: 'Pedro'
 }, {
-    id:4, 
-    name: 'Amanda'}];
+    id: 4,
+    name: 'Amanda'
+}];
 
 
-let userName = [];
+console.log(users[0].name.charAt(0));
 
-for (let i = 0, i < users.length, i++) {
-    if (users.name.uppercase.chartAt(0) === 'A'){
-        userName.push('Anacleto');
-    }else {
-        userName.push(users.name);
+let userNames = [];
+
+users.map((user) => {
+    let upperUser = user.name;
+    if (upperUser.charAt(0) === 'A') {
+        return userNames.push('Anacleto');
+    } else {
+        return userNames.push(user.name);
     }
-}
+});
+
+
+console.log(userNames);
